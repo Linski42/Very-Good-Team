acdaab08ad2edff55a4d30ad7101577f0c481ec4
Linski <connor.duke.molinski@gmail.com>
2022-11-16 14:02:56 -0800

    worked on Ant Pathing

--- src/AntStrategy/RobotPlayer.java
+++ src/AntStrategy/RobotPlayer.java
@@ -9,7 +9,7 @@ import java.util.Random;
  * is created!
  */
 public strictfp class RobotPlayer {
-
+    
     /**
      * We will use this variable to count the number of turns this robot has been alive.
      * You can use static variables like this to save any information you want. Keep in mind that even though
@@ -24,7 +24,6 @@ public strictfp class RobotPlayer {
      * we get the same sequence of numbers every time this code is run. This is very useful for debugging!
      */
     static final Random rng = new Random(6147);
-
     /** Array containing all the possible movement directions. */
     static final Direction[] directions = {
         Direction.NORTH,
@@ -41,16 +40,12 @@ public strictfp class RobotPlayer {
      * run() is the method that is called when a robot is instantiated in the Battlecode world.
      * It is like the main function for your robot. If this method returns, the robot dies!
      *
-     * @param rc  The RobotController object. You use it to perform actions from this robot, and to get
      *            information on its current status. Essentially your portal to interacting with the world.
      **/
     @SuppressWarnings("unused")
     public static void run(RobotController rc) throws GameActionException {
-
-        // Hello world! Standard output is very useful for debugging.
         // Everything you say here will be directly viewable in your terminal when you run a match!
         System.out.println("I'm a " + rc.getType() + " and I just got created! I have health " + rc.getHealth());
-
         // You can also use indicators to save debug notes in replays.
         rc.setIndicatorString("Hello world!");
 
@@ -109,13 +104,11 @@ public strictfp class RobotPlayer {
         // Pick a direction to build in.
         Direction dir = directions[rng.nextInt(directions.length)];
         if (rng.nextBoolean()) {
-            // Let's try to build a miner.
             rc.setIndicatorString("Trying to build a miner");
             if (rc.canBuildRobot(RobotType.MINER, dir)) {
                 rc.buildRobot(RobotType.MINER, dir);
             }
         } else {
-            // Let's try to build a soldier.
             rc.setIndicatorString("Trying to build a soldier");
             if (rc.canBuildRobot(RobotType.SOLDIER, dir)) {
                 rc.buildRobot(RobotType.SOLDIER, dir);
@@ -128,9 +121,10 @@ public strictfp class RobotPlayer {
      * This code is wrapped inside the infinite loop in run(), so it is called once per turn.
      */
     static void runMiner(RobotController rc) throws GameActionException {
+        int followID = -1;
         // Try to mine on squares around us.
-        MapLocation me = rc.getLocation();
         for (int dx = -1; dx <= 1; dx++) {
+            MapLocation me = rc.getLocation();
             for (int dy = -1; dy <= 1; dy++) {
                 MapLocation mineLocation = new MapLocation(me.x + dx, me.y + dy);
                 // Notice that the Miner's action cooldown is very low.
@@ -141,11 +135,12 @@ public strictfp class RobotPlayer {
                 while (rc.canMineLead(mineLocation)) {
                     rc.mineLead(mineLocation);
                 }
+
             }
+
         }
 
-        // Also try to move randomly.
-        Direction dir = directions[rng.nextInt(directions.length)];
+        Direction dir = Path.follow(rc, followID);
         if (rc.canMove(dir)) {
             rc.move(dir);
             System.out.println("I moved!");
