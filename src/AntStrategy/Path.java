a02715589178a106afde0cda6d3b6acbd63af733
Linski <connor.duke.molinski@gmail.com>
2022-11-29 14:25:51 -0800

    added path m

--- src/AntStrategy/Path.java
+++ src/AntStrategy/Path.java
@@ -5,7 +5,18 @@ import java.util.ArrayList;
 import battlecode.common.*;
 
 public interface Path {
-    public static Direction[] createPath(RobotController rc, MapLocation pos){
-        Direction[] out = {};
-        return out;}
+    static final Direction[] directions = {
+        Direction.NORTH,
+        Direction.NORTHEAST,
+        Direction.EAST,
+        Direction.SOUTHEAST,
+        Direction.SOUTH,
+        Direction.SOUTHWEST,
+        Direction.WEST,
+        Direction.NORTHWEST,
+    };
+    public static Direction goTo(RobotController rc, MapLocation pos){
+        Direction out = rc.getLocation().directionTo(new MapLocation(0, 0));
+        return out;
+    }
 }
